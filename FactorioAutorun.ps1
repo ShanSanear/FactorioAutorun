aecf14593b13daaa6264dde1227d9117f6675d79
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-08-02 08:48:34 +0200

    Added possibility to select save file to load

--- FactorioAutorun.ps1
+++ FactorioAutorun.ps1
@@ -1,11 +1,33 @@
 ﻿param(
     [Parameter(Mandatory = $true)][String] $SteamExe,
-    [Parameter(Mandatory = $true, ParameterSetName='SaveLoadType')][String] $SaveFolder
+    [Parameter(Mandatory = $true, ParameterSetName='SaveLoadType')][String] $SaveFolder,
+    [switch] $SelectSave = $false
 )
+# Based on https://thomasrayner.ca/open-file-dialog-box-in-powershell/
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "Save file (*.zip)| *.zip"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
 $FactorioAppID = 427520
 
 $Saves = Get-ChildItem -Path $SaveFolder
 
-$LastSave = $Saves | sort LastWriteTime | select -last 1
+if ($SelectSave) {
+    $SelectedSave = Get-FileName -InitialDirectory $SaveFolder
+    $SaveToLoad = $SelectedSave
+}
+else {
+    $LastSave = $Saves | sort LastWriteTime | select -last 1
+    $SaveToLoad = $LastSave.FullName
+}
 
-& $SteamExe -applaunch $FactorioAppID --load-game $LastSave.FullName
+
+
+& $SteamExe -applaunch $FactorioAppID --load-game $SaveToLoad
