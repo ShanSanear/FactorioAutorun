97d0a0c7360b92218813a768943e617f35ea7ed5
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-08-02 07:24:28 +0200

    Initial commit

---
+++ FactorioAutorun.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [Parameter(Mandatory = $true)][String] $SteamExe,
+    [Parameter(Mandatory = $true, ParameterSetName='SaveLoadType')][String] $SaveFolder
+)
+$FactorioAppID = 427520
+
+$Saves = Get-ChildItem -Path $SaveFolder
+
+$LastSave = $Saves | sort LastWriteTime | select -last 1
+
+& $SteamExe -applaunch $FactorioAppID --load-game $LastSave.FullName
