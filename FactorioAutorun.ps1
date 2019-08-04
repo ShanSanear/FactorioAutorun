a370cdd0acdb6ea9bd928e4ff1ad710c53faeb25
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-08-04 12:31:18 +0200

    Added handling for factorio.exe file and simple multiplayer host / connection.

--- FactorioAutorun.ps1
+++ FactorioAutorun.ps1
@@ -1,11 +1,31 @@
 ﻿param(
-    [Parameter(Mandatory = $true)][String] $SteamExe,
-    [Parameter(Mandatory = $true, ParameterSetName='SaveLoadType')][String] $SaveFolder,
-    [switch] $SelectSave = $false
+    [ValidateScript({
+        if (Test-Path $_ -PathType ‘Leaf’) {
+            return $True
+        }
+        else {
+            Throw "Incorrect execution file path: ${_}"
+        }
+    })]
+    [Parameter(Mandatory = $true)][String][Alias('SteamExe')] $Exe,
+
+    [ValidateScript({
+        if (Test-Path $_ -PathType ‘Container’) {
+            return $True
+        } 
+        else {
+            throw  "Incorrect path to save folder: ${_}"
+        }
+    })]
+    [Parameter(ParameterSetName='SaveFromFile')][String] $SaveFolder,
+
+    [switch] $SelectSave,
+    [switch] $Multiplayer,
+    [Parameter(ParameterSetName='JoinGame')][String] $HostIP
 )
+
 # Based on https://thomasrayner.ca/open-file-dialog-box-in-powershell/
-Function Get-FileName($initialDirectory)
-{
+Function Get-FileName($initialDirectory) {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
@@ -15,9 +35,21 @@ Function Get-FileName($initialDirectory)
     $OpenFileDialog.filename
 }
 
+$ExePath = Get-ChildItem $Exe.ToLower()
 $FactorioAppID = 427520
 
-$Saves = Get-ChildItem -Path $SaveFolder
+if ($ExePath.Name.Contains("steam.exe")) {
+    $ExeRun = "$($ExePath.FullName) -applaunch $FactorioAppID"
+} 
+elseif ($ExePath.Name.Contains("factorio.exe")) {
+    $ExeRun = $ExePath.FullName
+}
+else {
+    Write-Error "Incorrect exe file name: $($ExePath.FullName). Select either factorio.exe or steam.exe"
+    Exit
+}
+
+$Saves = Get-ChildItem $SaveFolder
 
 if ($SelectSave) {
     $SelectedSave = Get-FileName -InitialDirectory $SaveFolder
@@ -28,6 +60,16 @@ else {
     $SaveToLoad = $LastSave.FullName
 }
 
+if ($Multiplayer) {
+    if ($HostIP -ne "") {
+        $ExeParameters = "--mp-connect $HostIP --no-log-rotation"
+    }
+    else {
+        $ExeParameters = "--start-server $SaveToLoad --no-log-rotation"
+    }
+} 
+else {
+    $ExeParameters = "--load-game $SaveToLoad"
+}
 
-
-& $SteamExe -applaunch $FactorioAppID --load-game $SaveToLoad
+Start-Process $ExeRun $ExeParameters
